574ade2dd3ef754663d2370040db4dc3e77bc2d0
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-01-16 17:11:23 -0300

    Update test_tablas.py

--- test_tablas.py
+++ test_tablas.py
@@ -1,18 +1,9 @@
-# El objetivo del siguiente test es poder realizar las consultas solicitadas en un ambiente separado del programa principal con el objetivo de resguardar la información ya generada y mantener la autonomía de los procesos. 
- 
-import os
+### INTRO ###
 
+import os
 import pandas as pd
 
-
-# Directorio de bases a cargar
-dir_path = "/directorio/de/carpeta/donde/alojan/las/bases/"
-
-### Carga de tablas como base de datos:
-# * Se crea el directorio vacío "bases" en el cual se cargarán todas las bases.
-# * Se cargarán tantas bases como archivos csv hayan presentes en el directorio seleccionado. 
-# * Inicialmente quedarán con el nombre del archivo y aunque pueden ser editados no es estrictamente necesario
-
+dir_path = "/home/haze/Documentos/Programa/AtomProyects/RUCAS/tablas/TEST/test_consultas_SQL/Tablas/"
 
 bases = {}
 
@@ -22,22 +13,15 @@ for file in os.listdir(dir_path):
     if db.columns.shape[0] < 2:
         db = pd.read_csv(dir_path+file, sep=';', thousands=",", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
     bases[file.split('.')[0]] = db
-
-
-# ##### Ejemplo de base cargada: Se menciona el directorio, especifíca la base y llama a la acción "head()" que imprime los 5 primeros casos de la tabla completa.
-# No visualiza las 135 columnas por un tema de espacio:
-
+          
 bases["w1_bdm_e_beta"].head() 
 
-#Pregunta n° 1:
-
-##### Base con información de Brisas del Mar, de las 3 olas, que contenga las preguntas H3, H5, H9, H7, CS19, CS20, O19a, O20 y  para cada una de las viviendas. Las olas se agregan como columnas.
-##### Corresponde ahora seleccionar aquellas variables que si perduran en las distintas waves (para un mejor ejemplo)
-          
 for base in bases:
     bases[base]["folio_unico"] = bases[base]["folio_villa"].astype('str') + '-' + bases[base]["folio_vivienda"].astype('str')
 
-cols = ["H3", "H5", "H9", "H7", "CS19", "CS20", "O19a", "O20", "folio_unico"]
+### PARTE I ###
+
+cols = ["folio_unico", "H3", "H5", "H9", "H7", "CS19", "CS20", "O19a", "O20"]
 
 df1 = bases["w1_bdm_e_beta"].loc[:, ["folio_villa", "folio_vivienda"] + cols]
 df2 = bases["w2_bdm_e_beta"].loc[:, cols]
@@ -46,14 +30,11 @@ df = (df1.join(df2.set_index("folio_unico"), how="left", on="folio_unico", lsuff
          .join(df3.set_index("folio_unico"), how="left", on="folio_unico", lsuffix="", rsuffix="_02")
      )
 
-
 df.head()
-#df.to_csv('ejemplo1.csv', sep=',', encoding='utf-8', index = False)
+#df.to_csv('output_test1.csv', sep=',', encoding='utf-8', index = False)
 
 
-# Pregunta n°2:
-
-#### Base con informacion de Brisas del Mar, que para cada sujeto contenga las variables "SB3", "SU2_1", "CS19", "SA15a" (de la línea de base) y las preguntas "P1_1", "P1_2", "P11" (de las 3 olas). Las olas se agregan como columnas.
+#### PARTE II ###
 
 cols2 = ["folio_unico", "SB3", "SU2_1", "CS19", "SA15a"]
 cols3 = ["P1_1", "P1_2", "P11"] 
@@ -64,25 +45,16 @@ dfc = bases["w2_bdm_p"].loc[:, cols3]
 dfd = bases["w3_bdm_p"].loc[:, cols3]
 
 
-dfb1 = (dfa.join(dfb.set_index('folio_vivienda'), how="left", on="folio_vivienda", lsuffix="_00a", rsuffix="_01a")
-        .join(dfc.set_index('folio_unico'), how="left", on="folio_unico", lsuffix="", rsuffix="_02a")
+dfb1 = (dfa.join(dfb.set_index("folio_vivienda"), how="left", on="folio_vivienda", lsuffix="_00a", rsuffix="_01a")
+        .join(dfc.set_index("folio_unico"), how="left", on="folio_unico", lsuffix="", rsuffix="_02a")
         .join(dfd.set_index('folio_unico'), how="left", on="folio_unico", lsuffix="", rsuffix="_03a")
        )
 
 
 dfb1.head()
 #dfb1.to_csv('ejemplo2.csv', sep=',', encoding='utf-8', index = False)
-
-
-# Dentro de cada folio de vivienda integre los otros
-# Probar pegar DF con las bases
-
-# ### 2 errores a corregir:
-
-# * Averiguar por qué al crear el dfb1 "cols" deja de tomar los valores o no los identifica
-
           
-# Pregunta n°3:
+#### PARTE III ###
 
 #### Base con información de Brisas del Mar y Marta Brunet de la pauta de observación, todas las preguntas. Las olas y villas se agregan como filas. 
 
